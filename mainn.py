e580226af083533ca9929be82f2731ce3d08c125
Kobita Nergala <bigjapko@gmail.com>
2024-05-30 15:59:51 +0200

    mod 4

--- mainn.py
+++ mainn.py
@@ -2,7 +2,32 @@ import tkinter as tk
 import customtkinter as ctk
 from pytube import YouTube as yt
 
+def on_progress(stream,chunk, bytes_remaining):
+    total_size=stream.filesize
+    bytes_downloaded=total_size-bytes_remaining
+    percentage = bytes_downloaded / total_size * 100
+    print(percentage)
+    per=str(int(percentage))
+    pPercentage.configure(text=per)
+    pPercentage.update()
+    progressBar.set(per/100)
+    progressBar.update()
 
+
+
+def startDownload():
+    print("tu")
+    finish_label.configure(text="robing", text_color="yellow")
+    finish_label.update()
+    try:
+        ytLink=link.get()
+        ytObject=yt(ytLink,on_progress_callback=on_progress)
+        video=ytObject.streams.get_lowest_resolution()
+        video.download()
+    except:
+        finish_label.configure(text="ERROR!", text_color="red")
+    else:
+        finish_label.configure(text="SuCCESS!", text_color="green")
 
 
 ctk.set_appearance_mode("System")
@@ -13,6 +38,38 @@ app = ctk.CTk()
 
 app.geometry("720x480")
 app.title("Py YouYube Downloader 5")
+
+title= ctk.CTkLabel(app,text="Insert a youtube link")
+title.pack(padx=10, pady=10)
+
+
+url_var=ctk.StringVar()
+link=ctk.CTkEntry(app,width=350, height=40,textvariable=url_var)
+# link.insert(0,"https://www.youtube.com/watch?v=aAcHRQ-IE0Q")
+
+link.pack()
+
+pPercentage=ctk.CTkLabel(app,text="0%")
+pPercentage.pack();
+
+progressBar=ctk.CTkProgressBar(app,width=400)
+progressBar.set(0)
+progressBar.pack(padx=10,pady=10)
+
+
+
+
+
+finish_label=ctk.CTkLabel(app,text="")
+finish_label.pack()
+
+download=ctk.CTkButton(app, text="Start download", command=startDownload)
+download.pack();
+
+
+
+
+
 
 app.mainloop();
 
