6cd57d819647438cd9bf366b813a91f6eb4fabf1
Kobita Nergala <bigjapko@gmail.com>
2024-05-30 12:09:24 +0200

    initial ver

---
+++ mainn.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+import customtkinter as ctk
+from pytube import YouTube
+
+
+
+
+ctk.set_appearance_mode("System")
+
+ctk.set_default_color_theme("blue")
+
+app = ctk.CTk()
+
+app.geometry("720x480")
+app.title("Py YouYube Downloader")
+
+app.mainloop();
+
+
+
+
